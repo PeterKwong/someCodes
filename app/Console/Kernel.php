d3280c8921dfc879693ac2e6e9c1ee30e0f00de0
chill kwong <chillkwong@chills-MacBook-Pro.local>
2020-04-17 02:17:12 +0800

    2020_4_13

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends ConsoleKernel
             $CronJob->runResetAllRapDiamonds();
 
 
-        })->dailyAt('00:01')->runInBackground();
+        })->dailyAt('02:18')->runInBackground();
 
 
         $schedule->call(function () use(&$CronJob) {
