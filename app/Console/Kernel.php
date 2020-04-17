98eb1479c01b1ef53d4dfd3d574cb7813631587f
chill kwong <chillkwong@gmail.com>
2020-04-17 15:03:13 +0800

    2020_1_15

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends ConsoleKernel
 
             $this->diamondOncall($CronJob);
 
-        })->cron('*/4 * * * *')->between('00:31', '23:59')->runInBackground();
+        })->cron('*/5 * * * *')->between('00:31', '23:59')->runInBackground();
 
         $schedule->call(function () use(&$CronJob) {
             $CronJob->runCerts();
