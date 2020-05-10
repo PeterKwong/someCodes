5366402f509dd545c6013bf9fd71312fa4d1545b
chill kwong <chillkwong@gmail.com>
2020-05-10 15:44:27 +0800

    2020_4_15

--- resources/js/views/backEnd/invoiceDiamond/form.js
+++ resources/js/views/backEnd/invoiceDiamond/form.js
@@ -29,7 +29,7 @@ export default {
 		}
 		if ( window.location.pathname.includes('create-from-diamond/') ) {
 			this.title = 'Create'
-			this.initialize = '/api/invoice-diamonds/create-from-diamond/' + this.getIdReg(44)
+			this.initialize = '/api/invoice-diamonds/create-from-diamond/' + this.getIdReg(42)
 			this.store = '/api/invoiceDiamonds'
 			this.method = 'post'
 		}
