196f4662748c4acfd0b5c9f9cc7d8ded83cb788b
chill kwong <chillkwong@gmail.com>
2020-05-24 17:38:14 +0800

    2020_5_15

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title>test</title>
 	<style type="text/css">
-		@media print {
+		/*@media print {
 			     a[href*='//']:after {
 			        content:" (" attr(href) ") ";
 			        color: $primary;
@@ -12,19 +13,44 @@
 			width: 100%; 
 			margin: 0; 
 			float: none;
-			}
+			}*/
 			    
 			/** Seitenränder einstellen */       
-			@page { margin: 2cm }
-
+/*			@page { margin: 2cm }
+*/
 			@page {
 				  size: 2.36in 1.18in;
 				}
 
 	</style>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.js"></script>
 </head>
 <body>
-	<img src="/images/front-end/company/logo_2019_grey.png" width="50" >
-	test
+
+
+	<center>
+		<table>
+			<tr>
+				<td>
+					<img src="/images/front-end/company/logo_2019_grey.png" width="150" >
+				</td>
+				<td>
+					<div id="qrcode"></div>
+					<script type="text/javascript">
+					var qrcode = new QRCode(document.getElementById("qrcode"), {
+						text: "https://youtu.be/XvDUKOKUsRM",
+						width: 128,
+						height: 128,
+						colorDark : "#000000",
+						colorLight : "#ffffff",
+						correctLevel : QRCode.CorrectLevel.H
+					});
+					</script>	
+				</td>
+			</tr>
+		</table>
+		<a href="https://youtu.be/XvDUKOKUsRM">https://youtu.be/XvDUKOKUsRM</a> 	
+
+	</center>
 </body>
 </html>
