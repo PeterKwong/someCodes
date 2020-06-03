17953f0a9f150d20601fed95611dd73aa0a19ebc
chill kwong <chillkwong@gmail.com>
2020-06-03 18:54:30 +0800

    2020_6_3

--- resources/views/diamond/printLabel.blade.php
+++ resources/views/diamond/printLabel.blade.php
@@ -50,13 +50,13 @@
 				<div style="width: 30px; ">
 					<img src="/images/front-end/company/logo_2019_grey_sq.png" width="100%" >	
 				</div>
-				<div style="width: 120px;  font-size: 25px">$123141</div>
+				<div style="width: 100px;  font-size: 25px">$123141</div>
 			</div>
 			<div class="flex-container">
-				<div style="width: 150px; font-size: 20px">S138-ekjr123-123</div>
+				<div style="width: 130px; font-size: 20px">S138-ekjr123-123</div>
 			</div>
 			<div class="flex-container">
-				<div style="width: 150px;">1.01 , G , VVS2 </div>
+				<div style="width: 130px;">1.01 , G , VVS2 </div>
 			</div>
 		</div>
 		<div>
