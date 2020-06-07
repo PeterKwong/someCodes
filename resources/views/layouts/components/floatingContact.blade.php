11e87587631e4aa19bc43cc03805091b5eed8704
chill kwong <chillkwong@chills-MacBook-Pro.local>
2020-06-08 01:52:32 +0800

    2020_6_5

--- resources/views/layouts/components/floatingContact.blade.php
+++ resources/views/layouts/components/floatingContact.blade.php
@@ -1,4 +1,4 @@
-<a id="back-to-top" href="{{ 'https://api.whatsapp.com/send?phone=' . config( 'global.company.staffs.0.number') . '&text=' . urlencode( url()->current() ) }} " class="btn btn-primary btn-lg back-to-top sticker" role="button"><i class="far fa-comments"></i></a>
+<a id="back-to-top" href="{{ 'https://api.whatsapp.com/send?phone=852' . config( 'global.company.staffs.0.number') . '&text=' . urlencode( url()->current() ) }} " class="btn btn-primary btn-lg back-to-top sticker" role="button"><i class="far fa-comments"></i></a>
 
 
 
