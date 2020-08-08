cd4bae82b731b01ef6a253a46b62f43816ecf192
chill kwong <chillkwong@gmail.com>
2020-08-08 13:16:49 +0800

    2020_7_17

--- resources/views/layouts/metas/footer.blade.php
+++ resources/views/layouts/metas/footer.blade.php
@@ -1,4 +1,4 @@
 
-<script src="{{ mix('js/langs.js') }}" ></script>
-<script src="{{ mix('js/app.js') }}" ></script>
-<script src="{{ mix('js/frontend.js') }}" ></script>
+<script type="application/javascript" src="{{ mix('js/langs.js') }}" ></script>
+<script type="application/javascript" src="{{ mix('js/app.js') }}" ></script>
+<script type="application/javascript" src="{{ mix('js/frontend.js') }}" ></script>
