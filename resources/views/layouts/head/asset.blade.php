849ac3a10202ce5ec799449b527f6fc3ed203dbd
chill kwong <chillkwong@gmail.com>
2020-08-22 16:32:13 +0800

    2020_7_17

--- resources/views/layouts/head/asset.blade.php
+++ resources/views/layouts/head/asset.blade.php
@@ -31,8 +31,8 @@
  -->
 
 
-<script defer="" type="application/javascript" src="{{ mix('js/langs.js') }}" ></script>
-<script defer="" type="application/javascript" src="{{ mix('js/app.js') }}" ></script>
+<script type="application/javascript" src="{{ mix('js/langs.js') }}" ></script>
+<script type="application/javascript" src="{{ mix('js/app.js') }}" ></script>
 <script defer="" type="application/javascript" src="{{ mix('js/frontend.js') }}" ></script>
 
 
