d99735f44a70aaec014aab16b0020dc0cdd3f85b
chill kwong <chillkwong@chills-MacBook-Pro.local>
2020-09-17 03:38:01 +0800

    2020_9_7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,15 +60,27 @@ class Kernel extends ConsoleKernel
         //extra
         $schedule->call(function () use(&$CronJob) {
             $this->diamondOncall($CronJob);
-        })->cron('*/1 * * * *')->between('00:01', '00:35')->runInBackground();
+        })->cron('*/1 * * * *')->between('00:01', '00:30')->runInBackground();
 
         $schedule->call(function () use(&$CronJob) {
             $this->diamondOncall($CronJob);
-        })->cron('*/6 * * * *')->between('00:36', '02:00')->runInBackground();
+        })->cron('*/6 * * * *')->between('00:31', '01:30')->runInBackground();
 
         $schedule->call(function () use(&$CronJob) {
             $this->diamondOncall($CronJob);
-        })->cron('*/6 * * * *')->between('03:31', '05:00')->runInBackground();
+        })->cron('*/6 * * * *')->between('02:00', '03:00')->runInBackground();
+
+        $schedule->call(function () use(&$CronJob) {
+            $this->diamondOncall($CronJob);
+        })->cron('*/6 * * * *')->between('03:31', '04:30')->runInBackground();
+
+        $schedule->call(function () use(&$CronJob) {
+            $this->diamondOncall($CronJob);
+        })->cron('*/6 * * * *')->between('05:00', '06:00')->runInBackground();
+
+        // $schedule->call(function () use(&$CronJob) {
+        //     $this->diamondOncall($CronJob);
+        // })->cron('*/6 * * * *')->between('03:31', '05:00')->runInBackground();
 
 
 
