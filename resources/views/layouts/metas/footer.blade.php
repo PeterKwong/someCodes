f4413149678dd0a831997dd9bf3953c5c5f9052e
chill kwong <chillkwong@gmail.com>
2020-09-18 16:15:51 +0800

    2020_7_17

--- resources/views/layouts/metas/footer.blade.php
+++ resources/views/layouts/metas/footer.blade.php
@@ -1,2 +1,19 @@
 
 <link href="https://vjs.zencdn.net/7.8.3/video-js.css" rel="stylesheet" />
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "AggregateRating",
+  "itemReviewed": {
+    "@type": "Shop",
+    "image": "https://lh5.googleusercontent.com/p/AF1QipPUqwzVMGpEPZIn32pIWkW1LgbbGI8szltJyLYA=w100-h100-p-n-k-no",
+    "name": "Ting Diamond",
+    "telephone": "1234567",
+
+  },
+  "ratingValue": "4.8",
+  "bestRating": "5",
+  "ratingCount": "29"
+}
+</script>
