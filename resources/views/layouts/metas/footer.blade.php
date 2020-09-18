0c5508fefce86589fef001657b3f4f419a83229c
chill kwong <chillkwong@gmail.com>
2020-09-18 16:41:45 +0800

    2020_7_17

--- resources/views/layouts/metas/footer.blade.php
+++ resources/views/layouts/metas/footer.blade.php
@@ -23,4 +23,60 @@
   "bestRating": "5",
   "ratingCount": "29"
 }
+
+{
+  "@context": "https://schema.org",
+  "@type": "Jewellery Shop",
+  "@id": "{{ url()->current() }}",
+  "name": "Ting Diamond",
+  "image": "https://lh5.googleusercontent.com/p/AF1QipPUqwzVMGpEPZIn32pIWkW1LgbbGI8szltJyLYA=w100-h100-p-n-k-no",
+  "sameAs": "{{ url()->current() }}",
+  "priceRange": "$$$",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Room 202, 2/F, Knutsford Commercial Building, 4-5 Knutsford Terrace, Tsim Sha Tsui",
+    "addressLocality": "HK",
+    "addressRegion": "HK",
+    "postalCode": "000000",
+    "addressCountry": "HK"
+  },
+  "telephone": "+85254844533",
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 22.3012679,
+    "longitude": 114.1734194
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.8",
+    "bestRating": "5",
+    "worstRating": "4.8",
+    "ratingCount": "29"
+  },
+  "review": {
+    "@type": "Review",
+    "url": "{{ url()->current() }}",
+    "author": {
+      "@type": "Person",
+      "name": "Winnie",
+      "sameAs": "{{ url()->current() }}"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Winnie",
+      "sameAs": "{{ url()->current() }}"
+    },
+    "datePublished": "{{ today() }}",
+    "description": "(Translated by Google) The clerk is attentive.
+I bought a ring for the first time, and I don’t have any relevant knowledge. The clerk will answer all your questions in detail👍
+Worth recommending👍👍",
+    "inLanguage": "en",
+    "reviewRating": {
+      "@type": "Rating",
+      "worstRating": 4.8,
+      "bestRating":5,
+      "ratingValue": 4.8
+    }
+  }
+}
 </script>
