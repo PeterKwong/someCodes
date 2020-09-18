de96466ba1a430efd422ca20ebf4292fc59fa23d
chill kwong <chillkwong@chills-MacBook-Pro.local>
2020-09-19 02:04:39 +0800

    2020_9_7

--- app/Http/Support/Diamond/Sunrise.php
+++ app/Http/Support/Diamond/Sunrise.php
@@ -79,11 +79,11 @@ trait Sunrise{
               $d->parvilion_angle = $diamond['PavAng'];
               $d->fluorescence = $diamond['Fls']?$diamond['Fls']:'None'; 
               $d->lab = $diamond['Lab'];
-              $d->location = $diamond['Location']; 
+              $d->location = $diamond['Location'] == 'Hong Kong'?'1Hong Kong':'2'; 
               $d->available = 1; 
               $d->image_link = $diamond['Other Image']?$diamond['Other Image']:null;
               $d->has_image = $diamond['Other Image']?1:null;
-              
+
               // $d->video_link = $diamond['Other Video']?$diamond['Other Video']:null; 
               // $d->has_video = $diamond['Other Video']?1:null; 
 
