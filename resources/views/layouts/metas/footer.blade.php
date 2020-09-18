5f1371641aaaa7e46d4c4c30762ee72b760b7f65
chill kwong <chillkwong@gmail.com>
2020-09-18 16:53:57 +0800

    2020_7_17

--- resources/views/layouts/metas/footer.blade.php
+++ resources/views/layouts/metas/footer.blade.php
@@ -7,7 +7,7 @@
 "@type":"Product",
 "url":"{{ url()->current() }}",
 "name":"{{ trans('home.webTitle') }}",
-"description": "{{ trans('home.meta1') }},
+"description": "{{ trans('home.meta1') }}",
 "image": "https://lh5.googleusercontent.com/p/AF1QipPUqwzVMGpEPZIn32pIWkW1LgbbGI8szltJyLYA=w100-h100-p-n-k-no",
 "review": [
     {
