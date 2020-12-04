c4093b94769fe03af7660af7393d4c18c0872c74
chill kwong <chillkwong@gmail.com>
2020-12-04 15:34:57 +0800

    2020_12_3

--- resources/views/diamond/searchMobileLW.blade.php
+++ resources/views/diamond/searchMobileLW.blade.php
@@ -66,10 +66,10 @@
           <div class="hover:text-blue-600" x-on:click="selectDisplayColumn('color')">
               <div class="font-light text-lg">
                 <p class="flex justify-center ">
-                  <span class="btn btn-primary" :class=" `${search_conditions.color.Fancy.clicked?'text-blue-400 bg-white box':''}` "  x-on:click="search_conditions.color.Fancy.clicked = false ; resetFancyData(); displayColumn = '' ">
+                  <span class="btn btn-primary" :class=" `${search_conditions.color.Fancy.clicked?'text-blue-400 bg-white box':''}` "  x-on:click="search_conditions.color.Fancy.clicked = !search_conditions.color.Fancy.clicked ; resetFancyData(); displayColumn = '' ">
                     <span wire:click="toggleValue('color', 'Fancy' )">{{trans('diamondSearch.White')}}</span>
                   </span>
-                  <span class="btn btn-primary" :class=" `${search_conditions.color.Fancy.clicked?'':'text-blue-400 bg-white box'}` "  x-on:click="search_conditions.color.Fancy.clicked = true; resetFancyData(); displayColumn = '' ">
+                  <span class="btn btn-primary" :class=" `${search_conditions.color.Fancy.clicked?'':'text-blue-400 bg-white box'}` "  x-on:click="search_conditions.color.Fancy.clicked = !search_conditions.color.Fancy.clicked; resetFancyData(); displayColumn = '' ">
                     <span wire:click="toggleValue('color', 'Fancy' )">{{trans('diamondSearch.Fancy')}}</span>
                   </span>
                 </p>
