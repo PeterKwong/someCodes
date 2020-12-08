df929fdcc600bc999cc6ca882d6672006160ffe7
chill kwong <chillkwong@chills-MacBook-Pro.local>
2020-12-09 00:43:05 +0800

    2020_12_5

--- resources/lang/hk/page.php
+++ resources/lang/hk/page.php
@@ -14,8 +14,31 @@ return [
 
 	'metaTitle3' => '香港鑽石鑑定機構',
 	'metaDescription3' => '香港鑽石鑑定機構 - 如果你對鑽石有什麼懷疑，最好都是找一些第三方獨立機構。你可以找一些香港認可的機構，或是如果有證書，例如：GIA，你亦可以找他們為鑽石做一些覆檢。獲 香 港 認 可 處 認 可 的 硬 玉 質 翡 翠 或 鑽 石 測 試 實 驗 所 下 表 載 列 截 至 2 0 1 5 年 1 月 獲 香 港 認 可 處 認 可 能 夠 提 供 硬 玉 質 翡 翠 或 鑽 石 測 試 的 實 驗 所 名 單 ：',
+
+
 	'Hong Kong Sell Your Diamond Price' => 	'香港鑽石回收價格',
-	'Hong Kong Sell Your Diamond Price' => 	'香港鑽石回收價格',
+	'How to deal with some unwanted diamond jewelry'=>'如何處置不再配戴的鑽石首飾',
+	'For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable' => '對於家中一些已過時、陳舊或不再配戴的鑽石首飾感到婉惜? 與其由得它們閒置在家或保險箱，不如嘗試以下方法, 令其重新得到主人寵愛吧',
+	'Method'=>'方法',
+	'If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.'=>'如果戒指是經典簡約款式，只是因配戴而日久失色，你可以為戒指翻新電鍍及清潔。只需花費數百元，就可以變成全新的戒指。',
+	'Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.'=>'若然鑽石首飾款式已過時或嚴重變形，可以將它改鑲成戒指、吊咀、耳環、手鏈等，換成現時流行或經典的款式，甚至訂造屬於自己獨一無二的首飾。',
+	'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.'=>'倘若已不太喜歡所擁有的鑽石，也可以將不再配戴的鑽石首飾套現, 或者將小鑽石補差價，換成較大顆的鑽石。Ting Diamond 可為客人提供免費專業的鑽石估價服務，歡迎向我們查詢。',
+	'Diamond Resell Procedures'=>'鑽石回收的流程',
+	'With GIA Certificate'=>'有GIA證書​',
+	'Loose diamond or diamond jewelry.'=>'裸鑽或鑽飾皆可​',
+	'Valuation based on 4C info'=>'按證書4C條件給予估價​',
+	'Inclusion plot, fluorescence effect will also affect the value.'=>'I內含物位置、螢光等也會影響價錢​',
+	'Magnified diamond photo would help valuation.'=>'如有鑽石放大的相片，估價會更準確​',
+	'Close the deal'=>'價錢合適 即可交收​',
+	'For high clarity e.g. VS2 or above, GIA verification is required. The verification fee will be paid by us if the result is consistent with the GIA report.'=>'淨度較高 eg. VS2或以上，需到GIA覆檢，如檢測結果相符，費用由本公司負責',
+	'Cash Payment'=>'現金交易',
+
+	'Requirement'=>'注意事項',
+	'The diamond must be in its original condition with original diamond grading report.'=>'	閣下的鑽石必須是維持原好狀況，而且具備原來的鑽石鑑定證書。',
+	'Any damage to the original diamond will not be accepted.'=>'任何遭毀壞的鑽石都不會被接納。',
+	'If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.'=>'1卡或以上的鑽石如沒有GIA證書, 客人若願意到GIA Lab出證書，我們也會回收，歡迎向我們查詢詳情。',
+	'Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.'=>'本公司保留權利就有關條款進行加以詮釋。如有任何問題或爭議，本公司保留最終決定權。',
+
 
 	
 ];
--- resources/views/page/sellYourDiamonds.blade.php
+++ resources/views/page/sellYourDiamonds.blade.php
@@ -30,30 +30,60 @@
 
         <div id="home" class="p-4">
                        
-                <h1 class="text-2xl text-center">{{__('page.How to deal with some unwanted diamond jewelry')}}?</h1>
-                <h3>{{__('page.For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable')}}?</h3>
-                <h3>{{__('page.Method')}} 1 : </h3>
-                <p>{{__('page.If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.')}}
-                </p>
+                <h1 class="text-2xl text-center">{{__('page.How to deal with some unwanted diamond jewelry')}} || {{trans('page.Hong Kong Sell Your Diamond Price')}}?</h1>
+                <h3 class="p-2 text-xl text-gray-800 text-center">{{__('page.For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable')}}?</h3>
 
-                <h3>{{__('page.Method')}} 2 : </h3>
-                <p>{{__('page.Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.')}}
-                </p>
+                <div class="p-4">
+                    <h3 class="p-4 text-xl text-gray-800">{{__('page.Method')}} 1 : </h3>
+                    <p class="text-base text-gray-600">{{__('page.If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.')}}
+                    </p>
 
-                <h3>{{__('page.Method')}} 3 : </h3>
-                <p>{{__('page.In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.')}}
-                </p>
+                    <h3 class="p-4 text-xl text-gray-800">{{__('page.Method')}} 2 : </h3>
+                    <p class="text-base text-gray-600">{{__('page.Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.')}}
+                    </p>
+                    <img src="/images/front-end/sell_your_diamonds/broken ring.jpg">
 
-                <h2>{{__('page.Diamond Resell Procedures')}} : </h2>
+                    <h3 class="p-4 text-xl text-gray-800">{{__('page.Method')}} 3 : </h3>
+                    <p class="text-base text-gray-600">{{__('page.In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.')}}
+                    </p>
 
-                <h2>{{__('page.Requirement')}} : </h2>
-                <p>{{__('page.The diamond must be in its original condition with original diamond grading report.')}} </p>
-                <p>{{__('page.Any damage to the original diamond will not be accepted.')}} </p>
-                <p>{{__('page.If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.')}} </p>
-                <p>{{__('page.Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.')}} </p>
-                <p>{{__('page.Method')}} </p>
-                <p>{{__('page.Method')}} </p>
-                <p>{{__('page.Method')}} </p>
+                </div>
+
+                <div class="p-8">
+                    <h2 class="p-2 text-xl text-gray-900">{{__('page.Diamond Resell Procedures')}} : </h2>
+
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 sm:col-span-4">
+                            <img class="" src="/images/front-end/sell_your_diamonds/diamond cert.jpg">
+                             <h2 class="p-2 text-xl text-gray-900">{{__('page.With GIA Certificate')}}</h2>
+                            <p class="text-base text-gray-600">{{__('page.Loose diamond or diamond jewelry.')}}</p>
+                        </div>
+                        <div class="col-span-12 sm:col-span-4">
+                            <img class="" src="/images/front-end/sell_your_diamonds/diamond quote.jpg">
+                            <h2 class="p-2 text-xl text-gray-900">{{__('page.Valuation based on 4C info')}}</h2>
+                            <p class="text-base text-gray-600">{{__('page.Inclusion plot, fluorescence effect will also affect the value.')}}</p>
+                            <p class="text-base text-gray-600">{{__('page.Magnified diamond photo would help valuation.')}}</p>
+                        </div>
+                        <div class="col-span-12 sm:col-span-4">
+                            <img class="" src="/images/front-end/sell_your_diamonds/diamond cert.jpg">
+                            <h2 class="p-2 text-xl text-gray-900">{{__('page.Close the deal')}}</h2>
+                            <p class="text-base text-gray-600">{{__('page.For high clarity e.g. VS2 or above, GIA verification is required. The verification fee will be paid by us if the result is consistent with the GIA report.')}}</p>
+                            <p class="text-base text-gray-600">{{__('page.Cash Payment')}}</p>
+                        </div>
+                    </div>
+                   
+                </div>
+                
+
+                
+
+                
+
+                <h2 class="p-2 text-xl text-gray-900">{{__('page.Requirement')}} : </h2>
+                <p class="text-base text-gray-600">{{__('page.The diamond must be in its original condition with original diamond grading report.')}} </p>
+                <p class="text-base text-gray-600">{{__('page.Any damage to the original diamond will not be accepted.')}} </p>
+                <p class="text-base text-gray-600">{{__('page.If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.')}} </p>
+                <p class="text-base text-gray-600">{{__('page.Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.')}} </p>
 
 
         </div>
