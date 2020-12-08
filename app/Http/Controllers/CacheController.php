a91c1e7e172060db25d068a69b3028d99a9f79f4
chill kwong <chillkwong@gmail.com>
2020-12-08 17:35:49 +0800

    2020_12_3

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class CacheController extends Controller
 {
 	public function index(){
-		return view('backend.purchase.cache');
+		return view('backEnd.purchase.cache');
 	}
 }
