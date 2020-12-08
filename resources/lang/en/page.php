91686d3bada6723bc91b1def4a6e320a88586a90
chill kwong <chillkwong@gmail.com>
2020-12-08 18:20:16 +0800

    2020_12_3

--- resources/lang/en/page.php
+++ resources/lang/en/page.php
@@ -12,5 +12,24 @@ First, we will divide the diamond\'s Clarity into two major categories, Inclusio
 	'metaTitle3' => 'Hong Kong Diamond Gemology Organization',
 	'metaDescription3' => 'Hong Kong Diamond Gemology Organization - If you have any doubts about the diamond, it is best to find some third-party independent agencies. You can find some recognized institutions in Hong Kong, or if you have a certificate, such as GIA, you can also find them to do some review for the diamond. Jade or Emerald or Diamond Testing Laboratory Approved by Hong Kong Accreditation Service The following table sets out the list of laboratories approved by the Hong Kong Accreditation Service (HKAS) as of January 25, 2011 that are capable of providing jadeite or diamond testing:',
 	
-	
+
+	'Hong Kong Sell Your Diamond Price'=>'Hong Kong Sell Your Diamond Price',
+	'How to deal with some unwanted diamond jewelry'=>'How to deal with some unwanted diamond jewelry',
+	'For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable' => 'For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable',
+	'Method'=>'Method',
+	'If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.'=>'If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.',
+	'Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.'=>'Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.',
+	'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.'=>'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.',
+	'Requirement'=>'Requirement',
+	'The diamond must be in its original condition with original diamond grading report.'=>'The diamond must be in its original condition with original diamond grading report.',
+	'Any damage to the original diamond will not be accepted.'=>'Any damage to the original diamond will not be accepted.',
+	'If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.'=>'If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.',
+	'Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.'=>'Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.',
+	'Method'=>'Method',
+	'Method'=>'Method',
+	'Method'=>'Method',
+	'Method'=>'Method',
+	'Method'=>'Method',
+	'Method'=>'Method',
+
 ];
--- resources/views/page/sellYourDiamonds.blade.php
+++ resources/views/page/sellYourDiamonds.blade.php
@@ -28,9 +28,34 @@
         <br>
 
 
-        <div id="home">
+        <div id="home" class="p-4">
                        
-                <h1 class="">How to deal with some unwanted diamond jewelry?</h1>
+                <h1 class="text-2xl text-center">{{__('page.How to deal with some unwanted diamond jewelry')}}?</h1>
+                <h3>{{__('page.For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable')}}?</h3>
+                <h3>{{__('page.Method')}} 1 : </h3>
+                <p>{{__('page.If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.')}}
+                </p>
+
+                <h3>{{__('page.Method')}} 2 : </h3>
+                <p>{{__('page.Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.')}}
+                </p>
+
+                <h3>{{__('page.Method')}} 3 : </h3>
+                <p>{{__('page.In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.')}}
+                </p>
+
+                <h2>{{__('page.Diamond Resell Procedures')}} : </h2>
+
+                <h2>{{__('page.Requirement')}} : </h2>
+                <p>{{__('page.The diamond must be in its original condition with original diamond grading report.')}} </p>
+                <p>{{__('page.Any damage to the original diamond will not be accepted.')}} </p>
+                <p>{{__('page.If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.')}} </p>
+                <p>{{__('page.Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.')}} </p>
+                <p>{{__('page.Method')}} </p>
+                <p>{{__('page.Method')}} </p>
+                <p>{{__('page.Method')}} </p>
+
+
         </div>
 
             
