d29e559aeb06dfaa64c48f84a11aa976f9037305
chill kwong <chillkwong@gmail.com>
2021-01-12 19:32:23 +0800

    2020_12_3

--- resources/lang/cn/page.php
+++ resources/lang/cn/page.php
@@ -14,6 +14,31 @@ return [
 
 	'metaTitle3' => '香港钻石鉴定机构',
 	'metaDescription3' => '香港钻石鉴定机构 - 如果你对钻石有什么怀疑，最好都是找一些第三方独立机构。你可以找一些香港认可的机构，或是如果有证书，例如：GIA，你亦可以找他们为钻石做一些覆检。获 香 港 认 可 处 认 可 的 硬 玉 质 翡 翠 或 钻 石 测 试 实 验 所 下 表 载 列 截 至 2 0 1 5 年 1 月 获 香 港 认 可 处 认 可 能 够 提 供 硬 玉 质 翡 翠 或 钻 石 测 试 的 实 验 所 名 单 ：',
-	
+
+
+	'Hong Kong Sell Your Diamond Price' => 	'香港钻石回收/卖钻石价格',
+	'How to deal with some unwanted diamond jewelry'=>'如何处置不再配戴的钻石首饰',
+	'For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable' => '对于家中一些已过时、陈旧或不再配戴的钻石首饰感到婉惜? 与其由得它们闲置在家或保险箱，不如尝试以下方法, 令其重新得到主人宠爱吧',
+	'Method'=>'方法',
+	'If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.'=>'如果戒指是经典简约款式，只是因配戴而日久失色，你可以为戒指翻新电镀及清洁。只需花费数百元，就可以变成全新的戒指。',
+	'Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.'=>'若然钻石首饰款式已过时或严重变形，可以将它改镶成戒指、吊咀、耳环、手链等，换成现时流行或经典的款式，甚至订造属于自己独一无二的首饰。',
+	'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.'=>'倘若已不太喜欢所拥有的钻石，也可以将不再配戴的钻石首饰套现, 或者将小钻石补差价，换成较大颗的钻石。Ting Diamond 可为客人提供免费专业的钻石回收估价服务，卖钻石欢迎向我们查询。',
+	'Diamond Resell Procedures'=>'钻石回收的流程',
+	'With GIA Certificate'=>'有GIA证书​',
+	'Loose diamond or diamond jewelry.'=>'裸钻或钻饰皆可​',
+	'Valuation based on 4C info'=>'按证书4C条件给予估价​',
+	'Inclusion plot, fluorescence effect will also affect the value.'=>'I内含物位置、荧光等也会影响价钱​',
+	'Magnified diamond photo would help valuation.'=>'如有钻石放大的相片，估价会更准确​',
+	'Close the deal'=>'价钱合适 即可交收​',
+	'For high clarity e.g. VS2 or above, GIA verification is required. The verification fee will be paid by us if the result is consistent with the GIA report.'=>'净度较高 eg. VS2或以上，需到GIA覆检，如检测结果相符，费用由本公司负责',
+	'Cash Payment'=>'现金交易',
+
+	'Requirement'=>'注意事项',
+	'The diamond must be in its original condition with original diamond grading report.'=>'	阁下的钻石必须是维持原好状况，而且具备原来的钻石鉴定证书。',
+	'Any damage to the original diamond will not be accepted.'=>'任何遭毁坏的钻石都不会被接纳。',
+	'If 1ct or above diamond is without any certification, we will purchase your diamond on the condition customer is willing to apply for certification in GIA Lab.'=>'1卡或以上的钻石如没有GIA证书, 客人若愿意到GIA Lab出证书，我们也会回收，欢迎向我们查询详情。',
+	'Ting Diamond Limited reserve the right to interpret these Terms of Use and decide on any questions or disputes arising under these Terms of Use.'=>'本公司保留权利就有关条款进行加以诠释。如有任何问题或争议，本公司保留最终决定权。',
+
+
 	
 ];
--- resources/lang/hk/page.php
+++ resources/lang/hk/page.php
@@ -16,13 +16,13 @@ return [
 	'metaDescription3' => '香港鑽石鑑定機構 - 如果你對鑽石有什麼懷疑，最好都是找一些第三方獨立機構。你可以找一些香港認可的機構，或是如果有證書，例如：GIA，你亦可以找他們為鑽石做一些覆檢。獲 香 港 認 可 處 認 可 的 硬 玉 質 翡 翠 或 鑽 石 測 試 實 驗 所 下 表 載 列 截 至 2 0 1 5 年 1 月 獲 香 港 認 可 處 認 可 能 夠 提 供 硬 玉 質 翡 翠 或 鑽 石 測 試 的 實 驗 所 名 單 ：',
 
 
-	'Hong Kong Sell Your Diamond Price' => 	'香港鑽石回收價格',
+	'Hong Kong Sell Your Diamond Price' => 	'香港鑽石回收/賣鑽石價格',
 	'How to deal with some unwanted diamond jewelry'=>'如何處置不再配戴的鑽石首飾',
 	'For some old-fashioned and broken diamond jewelry, it’s pity to leave them idle or throw them away. What about transforming them into new one or something valuable' => '對於家中一些已過時、陳舊或不再配戴的鑽石首飾感到婉惜? 與其由得它們閒置在家或保險箱，不如嘗試以下方法, 令其重新得到主人寵愛吧',
 	'Method'=>'方法',
 	'If the classic style jewelry looks unattractive just because of fading coating and scratches, it simply costs you few hundred HK dollars to renew them by polishing and coating.'=>'如果戒指是經典簡約款式，只是因配戴而日久失色，你可以為戒指翻新電鍍及清潔。只需花費數百元，就可以變成全新的戒指。',
 	'Some jewelry setting may be out-of-fashion or deformed, you can have a new ring, pendant, earring or bracelet by buying a new trendy setting or even tailor made a unique style for yourself.'=>'若然鑽石首飾款式已過時或嚴重變形，可以將它改鑲成戒指、吊咀、耳環、手鏈等，換成現時流行或經典的款式，甚至訂造屬於自己獨一無二的首飾。',
-	'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.'=>'倘若已不太喜歡所擁有的鑽石，也可以將不再配戴的鑽石首飾套現, 或者將小鑽石補差價，換成較大顆的鑽石。Ting Diamond 可為客人提供免費專業的鑽石估價服務，歡迎向我們查詢。',
+	'In case you think it’s time to say good bye to the old diamond, you can sell your diamond to jeweler or even upgrade your diamond. Ting Diamond provides free professional diamond valuation, you may contact us for more details.'=>'倘若已不太喜歡所擁有的鑽石，也可以將不再配戴的鑽石首飾套現, 或者將小鑽石補差價，換成較大顆的鑽石。Ting Diamond 可為客人提供免費專業的鑽石回收估價服務，賣鑽石歡迎向我們查詢。',
 	'Diamond Resell Procedures'=>'鑽石回收的流程',
 	'With GIA Certificate'=>'有GIA證書​',
 	'Loose diamond or diamond jewelry.'=>'裸鑽或鑽飾皆可​',
