212add334ade7406e174b1149edab650f07303a5
chill kwong <chillkwong@gmail.com>
2021-02-24 15:31:58 +0800

    2020_12_3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,22 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+
+        "./resources/js/components/*.vue", 
+        "./resources/js/components/**/*.vue", 
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors:{
+                'primary':'#007bff',
+            },
+            transitionTimingFunction: {
+               'ease-in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+               'ease-out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
             },
         },
     },
@@ -19,6 +29,11 @@ module.exports = {
         extend: {
             opacity: ['disabled'],
         },
+        backgroundColor:['responsive', 'hover','focus','active'],
+        fontSize:['responsive', 'hover','focus','active'],
+        textColor:['responsive', 'hover','focus','active'],
+        translate: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        transitionTimingFunction: ['responsive', 'hover', 'focus'],
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
