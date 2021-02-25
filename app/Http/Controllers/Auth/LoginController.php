f0425d60ef40bb2e53225ce1cb96ffcd6db2ae9e
Chill <chillkwong@gmail.com>
2021-02-26 00:08:55 +0800

    2020_12_5

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -71,7 +71,7 @@ class LoginController extends Controller
     public function handleProviderCallback($provider)
     {
         $user = Socialite::driver($provider)->user();
-        // dd($user);
+        dd($user);
 
         $authUser = $this->findOrCreateUser($user, $provider);
         Auth::login($authUser, true);
