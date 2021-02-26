3b8bcbc725188e2e5a747381f2ceb54bd66fa9be
chill kwong <chillkwong@gmail.com>
2021-02-26 15:52:14 +0800

    2020_12_3

--- app/Http/Support/Diamond/Oncall.php
+++ app/Http/Support/Diamond/Oncall.php
@@ -112,7 +112,7 @@ trait Oncall{
                         
     public function getSupplierTotalStones($selectedID = 'white_diamond'){
 
-      // dd($data);
+      dd(env('OCID'));
 
       $data = $this->guzzleRequest($this->diamondSource,$selectedID,1);
       dd($data);
@@ -204,7 +204,7 @@ trait Oncall{
       $diamondSource[$selectedID]['data']['startindex'] = $p;
       $client = new Client();
 
-      dd(print_r($diamondSource[$selectedID]['data']['password']));
+      // dd(print_r($diamondSource[$selectedID]['data']['password']));
       $request = new Req($diamondSource[$selectedID]['method'], $diamondSource[$selectedID]['url'], $diamondSource[$selectedID]['header'] ,json_encode($diamondSource[$selectedID]['data']));
 
       $data = 0;
