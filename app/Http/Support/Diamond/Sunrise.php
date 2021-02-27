4040dda1c3f8d5347433524454e82b816a17ab07
chill kwong <chillkwong@gmail.com>
2021-02-27 12:31:06 +0800

    2020_12_3

--- app/Http/Support/Diamond/Sunrise.php
+++ app/Http/Support/Diamond/Sunrise.php
@@ -85,7 +85,8 @@ trait Sunrise{
               $d->parvilion_angle = $diamond['PavAng']?$diamond['PavAng']:null;
               $d->fluorescence = $diamond['Fls']?$diamond['Fls']:'None'; 
               $d->lab = $diamond['Lab'];
-              $d->location = $diamond['Location'] == 'Hong Kong' && $diamond['Cts'] > 1.0 && $diamond['Net Amt($)'] >3000 ?'1Hong Kong':'2'; 
+              $d->location = '2'; 
+              // $d->location = $diamond['Location'] == 'Hong Kong' && $diamond['Cts'] > 1.0 && $diamond['Net Amt($)'] >3000 ?'1Hong Kong':'2'; 
               $d->available = 1; 
               // $d->image_link = $diamond['View Image']?$diamond['View Image']:null;
               // $d->has_image = $diamond['View Image']?1:null;
