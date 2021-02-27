044d43fb94d35d3c3b556ead8c4d9d9de356faf7
chill kwong <chillkwong@gmail.com>
2021-02-27 15:01:39 +0800

    2020_12_3

--- resources/views/backEnd/engagementRing/form.blade.php
+++ resources/views/backEnd/engagementRing/form.blade.php
@@ -241,7 +241,7 @@
 					<div class="col">
 							<div class="box">
 								<label> video 360</label>
-								<p>@{{form.video306}} 123</p>
+								<p>@{{form.video360}}</p>
 								<small class="error__control" v-if="errors.cover">@{{errors.cover[0]}}</small>
 							</div>
 					</div>
