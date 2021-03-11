d49fdf53897cbe026c68a767fe7e04679a97bce3
Chill <chillkwong@gmail.com>
2021-03-12 02:23:10 +0800

    2020_12_5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -44,6 +44,11 @@ class Kernel extends ConsoleKernel
             Cache::put('counter',1, 90000);
             Cache::put('diamondQueryState',0,90000);
             
+        })->dailyAt('00:00')->runInBackground();
+
+
+        $schedule->call(function () use(&$CronJob) {
+
             $CronJob->runImportDiamondRap();
             $CronJob->runImportDiamondSunrise();
 
@@ -51,7 +56,7 @@ class Kernel extends ConsoleKernel
             $CronJob->runResetAllRapDiamonds();
 
 
-        })->dailyAt('02:17')->runInBackground();
+        })->dailyAt('00:01')->runInBackground();
 
 
         $schedule->call(function () use(&$CronJob) {
