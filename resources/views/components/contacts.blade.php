9c437f8b327d632bf865dccd30ca5ce780792713
chill kwong <chillkwong@gmail.com>
2021-03-18 14:36:39 +0800

    2021_3_9

--- resources/views/components/contacts.blade.php
+++ resources/views/components/contacts.blade.php
@@ -5,27 +5,29 @@
         <div class="flex justify-center">
 
         @if(app()->getLocale() != 'cn')
-          <div class="p-2">
-            <div class="w-12 h-12">
-              <img src="{{ '/images/front-end/contact/' . $staff['name'] . '.png' }}" class="w-12 h-12 rounded-full flex items-center justify-center" alt="...">
-            </div>
-          </div>
-	        <div class="card-body">
-	       		<div class="flex items-center text-center">
-		          <img class="w-8 h-8 m-1" src="/images/front-end/aboutUs/whatsapp.jpg">
-		          <h5 class="pt-2 text-2xl text-gray-600">{{ $staff['name'] }}</h5>
-	        	</div>
+		    <a class="text-blue-600" href="{{ '/links/whatsapp/852' .  $staff['number'] }}">
+		    	<div class="flex">
+		    		<div class="p-2">
+			            <div class="w-12 h-12">
+			              <img src="{{ '/images/front-end/contact/' . $staff['name'] . '.png' }}" class="w-12 h-12 rounded-full flex items-center justify-center" alt="...">
+			            </div>
+			          </div>
+			        <div class="card-body">
+			       		<div class="flex items-center text-center">
+				          <img class="w-8 h-8 m-1" src="/images/front-end/aboutUs/whatsapp.jpg">
+				          <h5 class="pt-2 text-2xl text-gray-600">{{ $staff['name'] }}</h5>
+			        	</div>
 
-	            <p class="card-text">
-	              <small class="text-muted">
-	                  <a class="text-blue-600" href="{{ '/links/whatsapp/852' .  $staff['number'] }}">
-	                    {{__('aboutUs.or contact her via')}}
-	                    whatsapp
-	                  </a>
-	              </small>
-	            </p>
-	        </div>
-          @endif
+			            <p class="card-text">
+			              <small class="text-muted">
+			                    {{__('aboutUs.or contact her via')}}
+			                    whatsapp
+			              </small>
+			            </p>
+			        </div>
+		    	</div>
+		     </a>
+         @endif
 
         @if(app()->getLocale() == 'cn')
 	      <div class="p-2">
