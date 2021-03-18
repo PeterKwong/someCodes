84f0561cc31317ac5e0dd3db56751ef18374439a
chill kwong <chillkwong@gmail.com>
2021-03-18 14:38:05 +0800

    2021_3_9

--- resources/views/frontend/aboutUs/buyingProcedure.blade.php
+++ resources/views/frontend/aboutUs/buyingProcedure.blade.php
@@ -8,13 +8,13 @@
 
         <!-- Schema.org markup for Google+ --> 
         <meta itemprop="name" content="{{trans('buyingProcedure.metaTitle1')}} - {{trans('home.webTitle')}}"> 
-        <meta itemprop="description" content="{{trans('buyingProcedure.metaDescription1')}}""> 
-        <meta itemprop="image" content="{{url("/front_end/home/h1_300-1.png")}}">
+        <meta itemprop="description" content="{{trans('buyingProcedure.metaDescription1')}}"> 
+        <meta itemprop="image" content="{{url('/front_end/home/h1_300-1.png')}}">
 
         <!-- Open Graph data --> 
         <meta property="og:title" content="{{trans('buyingProcedure.metaTitle1')}} - {{trans('home.webTitle')}}" /> 
-        <meta property="og:url" content="{{url("/")}}" />
-        <meta property="og:image" content="{{url("/front_end/home/h1_300-1.png")}}" />
+        <meta property="og:url" content="{{ url()->current() }}" />
+        <meta property="og:image" content="{{url('/front_end/home/h1_300-1.png')}}" />
         <meta property="og:description" content="{{trans('buyingProcedure.metaDescription1')}}" /> 
         <meta property="og:site_name" content="{{trans('buyingProcedure.metaTitle1')}} - {{trans('home.webTitle')}}" /> 
         <meta property="article:published_time" content="2013-09-17T05:59:00+01:00" /> 
