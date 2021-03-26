0ba17dd25a07b3d9369f05c3e3aa27e505555fc6
chill kwong <chillkwong@gmail.com>
2021-03-26 18:42:54 +0800

    2021_3_20

--- resources/views/frontend/shoppingCart/thankYouPage.blade.php
+++ resources/views/frontend/shoppingCart/thankYouPage.blade.php
@@ -8,24 +8,24 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Place this data between the <head> tags of your website --> 
-        <title>@include('frontend.shoppingCart.meta') - {{trans('home.webTitle')}}</title>
-        <meta name="description" content="@include('frontend.shoppingCart.meta') {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}" />
-        <meta name="keywords" content="@include('frontend.shoppingCart.keywords')"> 
+        <title> - {{trans('home.webTitle')}}</title>
+        <meta name="description" content=" {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}" />
+        <meta name="keywords" content=""> 
 
         <!-- Schema.org markup for Google+ --> 
-        <meta itemprop="name" content="@include('frontend.shoppingCart.meta') - {{trans('home.webTitle')}}"> 
-        <meta itemprop="description" content="@include('frontend.shoppingCart.meta') {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}""> 
+        <meta itemprop="name" content=" - {{trans('home.webTitle')}}"> 
+        <meta itemprop="description" content=" {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}""> 
         <meta itemprop="image" content="{{url("/front_end/home/h1_300-1.png")}}">
 
         <!-- Open Graph data --> 
-        <meta property="og:title" content="@include('frontend.shoppingCart.meta') - {{trans('home.webTitle')}}" /> 
+        <meta property="og:title" content=" - {{trans('home.webTitle')}}" /> 
         <meta property="og:url" content="{{url("/")}}" />
         <meta property="og:image" content="{{url("/front_end/home/h1_300-1.png")}}" />
         <meta property="og:description" content="{{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}" /> 
-        <meta property="og:site_name" content="@include('frontend.shoppingCart.meta') - {{trans('home.webTitle')}}" /> 
+        <meta property="og:site_name" content=" - {{trans('home.webTitle')}}" /> 
         <meta property="article:published_time" content="2013-09-17T05:59:00+01:00" /> 
         <meta property="article:modified_time" content="2013-09-16T19:08:47+01:00" />
-        <meta property="article:tag" content="@include('frontend.shoppingCart.keywords')" /> 
+        <meta property="article:tag" content="" /> 
  
 
     @endSection
