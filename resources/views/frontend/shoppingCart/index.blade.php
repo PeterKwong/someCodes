c98d94b28929330f261c25fadeb127177f439aac
chill kwong <chillkwong@gmail.com>
2021-03-26 18:48:20 +0800

    2021_3_20

--- resources/views/frontend/shoppingCart/index.blade.php
+++ resources/views/frontend/shoppingCart/index.blade.php
@@ -8,21 +8,21 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Place this data between the <head> tags of your website --> 
-        <title> - {{trans('home.webTitle')}}</title>
+        <title> {{trans('home.webTitle')}}</title>
         <meta name="description" content=" {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}" />
         <meta name="keywords" content=""> 
 
         <!-- Schema.org markup for Google+ --> 
-        <meta itemprop="name" content=" - {{trans('home.webTitle')}}"> 
+        <meta itemprop="name" content="{{trans('home.webTitle')}}"> 
         <meta itemprop="description" content=" {{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}""> 
         <meta itemprop="image" content="{{url("/front_end/home/h1_300-1.png")}}">
 
         <!-- Open Graph data --> 
-        <meta property="og:title" content=" - {{trans('home.webTitle')}}" /> 
+        <meta property="og:title" content="{{trans('home.webTitle')}}" /> 
         <meta property="og:url" content="{{url("/")}}" />
         <meta property="og:image" content="{{url("/front_end/home/h1_300-1.png")}}" />
         <meta property="og:description" content="{{trans('engagementRing.metaDescription1')}} - {{trans('home.meta2')}}" /> 
-        <meta property="og:site_name" content=" - {{trans('home.webTitle')}}" /> 
+        <meta property="og:site_name" content="{{trans('home.webTitle')}}" /> 
         <meta property="article:published_time" content="2013-09-17T05:59:00+01:00" /> 
         <meta property="article:modified_time" content="2013-09-16T19:08:47+01:00" />
         <meta property="article:tag" content="" /> 
