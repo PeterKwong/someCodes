3572fd58afe005d36e98e7c66e4acc7666b25f0e
chill kwong <chillkwong@gmail.com>
2021-04-20 18:21:45 +0800

    2021_3_20g

--- routes/sitemapRoutes.php
+++ routes/sitemapRoutes.php
@@ -2,10 +2,7 @@
 
 use Carbon\Carbon;
 
-    $sitemap = App::make('sitemap');
-
-	$sitemap->setCache('tingdiamond.sitemap', 10);
-
+    $combinedSitemap = App::make('sitemap');
 
 	$translations = [
 			['language' => 'en', 'url' => url()->to('/en/')],
@@ -13,205 +10,22 @@ use Carbon\Carbon;
 			['language' => 'zh-Hans', 'url' => url()->to('/cn/')],
 		];
 
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/pages/sitemap.xml');
 
-	cache()->put('sitemap.sitemapCounter', 0);
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/engagement-rings/sitemap.xml');
 
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/engagement-rings/sitemap.xml');
 
-	$pages = DB::table('pages')->orderBy('created_at', 'desc')
-		->chunk(1000,function($pages) use (&$sitemap,$translations){
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/wedding-rings/sitemap.xml');
 
-			// $counter = cache()->get('sitemap.counter');
-			$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-				// add every product to multiple sitemaps with one sitemap index
-				foreach ($pages as $p) {
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/jewelleries/sitemap.xml');
 
-					foreach ($translations as $trans) {
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/customer-posts/sitemap.xml');
 
-						$sitemap->add($trans['url'] . '/' . $p->url, now(), '0.9', 'daily');
+	$combinedSitemap->addSitemap( $url . 'vendor/sitemap/diamonds/sitemap.xml');
 
-					}
 
-					// count number of elements
-						// dd($sitemap);
-				}
-			
-			// dd($sitemap->model->getItems());
-
-			// dd($sitemap);
-			$sitemap->store('xml', 'vendor/sitemap/pages/sitemap-' . $sitemapCounter);
-			// add the file to the sitemaps array
-			$sitemap->addSitemap(secure_url('vendor/sitemap/pages/sitemap-' . $sitemapCounter . '.xml'));
-			// reset items array (clear memory)
-			$sitemap->model->resetItems();
-			// reset the counter
-			// $counter = 0;
-			// count generated sitemap
-
-			cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-	});
-
-	cache()->put('sitemap.sitemapCounter', 0);
-
-
-	$engagementRings = DB::table('engagement_rings')->where('published',1)->orderBy('created_at', 'desc')
-		->chunk(1000,function($engagementRings) use (&$sitemap,$translations){
-
-			// $counter = cache()->get('sitemap.counter');
-			$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-				// add every product to multiple sitemaps with one sitemap index
-				foreach ($engagementRings as $p) {
-
-					foreach ($translations as $trans) {
-
-						$sitemap->add($trans['url'] . '/engagement-rings/' . $p->id, now(), '0.8', 'daily');
-
-					}
-
-					// count number of elements
-						// dd($sitemap);
-				}
-			
-			// dd($sitemap->model->getItems());
-
-			// dd($sitemap);
-			$sitemap->store('xml', 'vendor/sitemap/engagement-rings/sitemap-' . $sitemapCounter);
-			// add the file to the sitemaps array
-			$sitemap->addSitemap(secure_url('vendor/sitemap/engagement-rings/sitemap-' . $sitemapCounter . '.xml'));
-			// reset items array (clear memory)
-			$sitemap->model->resetItems();
-			// reset the counter
-			// $counter = 0;
-			// count generated sitemap
-
-			cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-	});
-
-	cache()->put('sitemap.sitemapCounter', 0);
-
-	$weddingRings = DB::table('wedding_rings')->where('published',1)->orderBy('created_at', 'desc')
-		->chunk(1000,function($weddingRings) use (&$sitemap,$translations){
-
-			// $counter = cache()->get('sitemap.counter');
-			$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-				// add every product to multiple sitemaps with one sitemap index
-				foreach ($weddingRings as $p) {
-
-					foreach ($translations as $trans) {
-
-						$sitemap->add($trans['url'] . '/wedding-rings/' . $p->id, now(), '0.8', 'daily');
-
-					}
-
-					// count number of elements
-						// dd($sitemap);
-				}
-			
-			// dd($sitemap->model->getItems());
-
-			// dd($sitemap);
-			$sitemap->store('xml', 'vendor/sitemap/wedding-rings/sitemap-' . $sitemapCounter);
-			// add the file to the sitemaps array
-			$sitemap->addSitemap(secure_url('vendor/sitemap/wedding-rings/sitemap-' . $sitemapCounter . '.xml'));
-			// reset items array (clear memory)
-			$sitemap->model->resetItems();
-			// reset the counter
-			// $counter = 0;
-			// count generated sitemap
-
-			cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-	});
-
-	cache()->put('sitemap.sitemapCounter', 0);
-
-
-	$jewelleries = DB::table('jewelleries')->where('published',1)->orderBy('created_at', 'desc')
-		->chunk(1000,function($jewelleries) use (&$sitemap,$translations){
-
-			// $counter = cache()->get('sitemap.counter');
-			$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-				// add every product to multiple sitemaps with one sitemap index
-				foreach ($jewelleries as $p) {
-
-					foreach ($translations as $trans) {
-
-						$sitemap->add($trans['url'] . '/jewelleries/' . $p->id, now(), '0.8', 'daily');
-
-					}
-
-					// count number of elements
-						// dd($sitemap);
-				}
-			
-			// dd($sitemap->model->getItems());
-
-			// dd($sitemap);
-			$sitemap->store('xml', 'vendor/sitemap/jewelleries/sitemap-' . $sitemapCounter);
-			// add the file to the sitemaps array
-			$sitemap->addSitemap(secure_url('vendor/sitemap/jewelleries/sitemap-' . $sitemapCounter . '.xml'));
-			// reset items array (clear memory)
-			$sitemap->model->resetItems();
-			// reset the counter
-			// $counter = 0;
-			// count generated sitemap
-
-			cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-	});
-
-	cache()->put('sitemap.sitemapCounter', 0);
-
-	$customerPosts = DB::table('invoice_posts')->where('published',1)->orderBy('created_at', 'desc')
-		->chunk(1000,function($customerPosts) use (&$sitemap,$translations){
-
-			// $counter = cache()->get('sitemap.counter');
-			$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-				// add every product to multiple sitemaps with one sitemap index
-				foreach ($customerPosts as $p) {
-
-					foreach ($translations as $trans) {
-
-						$sitemap->add($trans['url'] . '/customer-posts/' . $p->id, now(), '0.8', 'daily');
-
-					}
-
-					// count number of elements
-						// dd($sitemap);
-				}
-			
-			// dd($sitemap->model->getItems());
-
-			// dd($sitemap);
-			$sitemap->store('xml', 'vendor/sitemap/customer-posts/sitemap-' . $sitemapCounter);
-			// add the file to the sitemaps array
-			$sitemap->addSitemap(secure_url('vendor/sitemap/customer-posts/sitemap-' . $sitemapCounter . '.xml'));
-			// reset items array (clear memory)
-			$sitemap->model->resetItems();
-			// reset the counter
-			// $counter = 0;
-			// count generated sitemap
-
-			cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-	});
-
-	cache()->put('sitemap.sitemapCounter', 0);
-
-
-	
-	$sitemap->addSitemap(secure_url('vendor/sitemap/diamonds/sitemap.xml'));
-
-	$sitemap->store('sitemapindex', 'sitemap_index');
-
-
-	return redirect('sitemap_index.xml');
+	return $sitemap->render('xml');
 
 
 
