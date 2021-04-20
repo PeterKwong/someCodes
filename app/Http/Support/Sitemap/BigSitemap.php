b5a19ac2ca047d8eac30a603dc1fd4d97fcfe28b
chill kwong <chillkwong@gmail.com>
2021-04-20 16:29:05 +0800

    2021_3_20g

--- app/Http/Support/Sitemap/BigSitemap.php
+++ app/Http/Support/Sitemap/BigSitemap.php
@@ -60,49 +60,6 @@ class BigSitemap
 
 	return redirect('sitemap_index.xml');
 
-		// // get all diamonds from db (or wherever you store them)
-		// $diamonds = DB::table('diamonds')->whereAvailable(1)->orderBy('created_at', 'desc')
-		// 	->chunk(1000,function($diamonds) use (&$sitemap,$translations,$url){
-
-		// 		// $counter = cache()->get('sitemap.counter');
-		// 		$sitemapCounter = cache()->get('sitemap.sitemapCounter');
-		// 			// add every product to multiple sitemaps with one sitemap index
-		// 			foreach ($diamonds as $p) {
-
-		// 				foreach ($translations as $trans) {
-
-		// 					$sitemap->add($trans['url'] . '/gia-loose-diamonds/' . $p->id, $p->updated_at, '0.5', 'monthly');
-
-		// 				}
-
-		// 				// count number of elements
-		// 					// dd($sitemap);
-		// 			}
-				
-		// 		// dd($sitemap->model->getItems());
-
-		// 		// dd($sitemap);
-		// 		$sitemap->store('xml', 'vendor/sitemap/diamonds/sitemap-' . $sitemapCounter);
-		// 		// add the file to the sitemaps array
-		// 		$sitemap->addSitemap($url .'vendor/sitemap/diamonds/sitemap-' . $sitemapCounter . '.xml');
-		// 		// reset items array (clear memory)
-		// 		$sitemap->model->resetItems();
-		// 		// reset the counter
-		// 		// $counter = 0;
-		// 		// count generated sitemap
-
-		// 		cache()->put('sitemap.sitemapCounter', $sitemapCounter + 1);
-
-
-		// });
-
-
-		// $sitemap->store('sitemapindex', 'vendor/sitemap/diamonds/sitemap');
-
-	
-
-
-
 
 	} 
 
