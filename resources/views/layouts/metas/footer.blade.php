f376583561c3ae7ef8ef5c69c6aaabfef434fa5b
chill kwong <chillkwong@gmail.com>
2021-06-03 03:26:51 +0800

    2021_3_20

--- resources/views/layouts/metas/footer.blade.php
+++ resources/views/layouts/metas/footer.blade.php
@@ -1,18 +1,20 @@
+<div>
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/color.css') }}">
 
-<link rel="stylesheet" type="text/css" href="{{ asset('css/color.css') }}">
-
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org/",
-  "@type": "Product",
-  "name": "Ting Diamond",
-  "image": [
-    "https://lh5.googleusercontent.com/p/AF1QipPUqwzVMGpEPZIn32pIWkW1LgbbGI8szltJyLYA=w100-h100-p-n-k-no"   
-    	],
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "{{ cache()->get('googleCommentRate') }}",
-    "reviewCount": "{{ cache()->get('googleCommentCount') }}"
+  <script type="application/ld+json">
+  {
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    "name": "Ting Diamond",
+    "image": [
+      "https://lh5.googleusercontent.com/p/AF1QipPUqwzVMGpEPZIn32pIWkW1LgbbGI8szltJyLYA=w100-h100-p-n-k-no"   
+        ],
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "{{ cache()->get('googleCommentRate') }}",
+      "reviewCount": "{{ cache()->get('googleCommentCount') }}"
+    }
   }
-}
-</script>
+  </script>
+</div>
+
