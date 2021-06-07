66a072822ff5da865520118908c76655f796f5ae
chillkwong <chillkwong@gmail.com>
2021-06-07 18:03:53 +0800

    2021_5_15

--- resources/js/components/productViewer360.vue
+++ resources/js/components/productViewer360.vue
@@ -10,45 +10,6 @@
 </style>
 
 <template>
-  <div>
- <!--    <div class="relative z-10" v-if="loading" >
-      <button tabindex="1" class="fixed inset-0 bg-black opacity-25 h-full w-full cursor-default">
-        <div class="flex justify-center z-20">
-          <svg width="100" height="100" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-              <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
-                  <stop stop-color="#fff" stop-opacity="0" offset="0%"/>
-                  <stop stop-color="#fff" stop-opacity=".631" offset="63.146%"/>
-                  <stop stop-color="#fff" offset="100%"/>
-              </linearGradient>
-          </defs>
-          <g fill="none" fill-rule="evenodd">
-              <g transform="translate(1 1)">
-                  <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2">
-                      <animateTransform
-                          attributeName="transform"
-                          type="rotate"
-                          from="0 18 18"
-                          to="360 18 18"
-                          dur="0.9s"
-                          repeatCount="indefinite" />
-                  </path>
-                  <circle fill="#fff" cx="36" cy="18" r="1">
-                      <animateTransform
-                          attributeName="transform"
-                          type="rotate"
-                          from="0 18 18"
-                          to="360 18 18"
-                          dur="0.9s"
-                          repeatCount="indefinite" />
-                  </circle>
-              </g>
-          </g>
-      </svg>
-        </div>
-      </button>
-    </div>
-     -->
     <div class="grid grid-cols-12 ">
           <div class="col-span-12">
             <canvas id="productViewer" @click="pauseOrStart()" class="flex" :width="width" :height="height" 
@@ -76,7 +37,6 @@
             </canvas>
           </div>            
     </div>
-  </div>
 </template>
 
 <script type="text/javascript">
