9bc293ecce5d113383a3be46389b4680e3d560c7
chillkwong <chillkwong@gmail.com>
2021-06-13 21:02:13 +0800

    2021_5_15

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -46,7 +46,6 @@
           </div>
 
 
-
         <div class="">
           <div class="flex justify-around pt-2 relative">
             <div class="flex-1 hidden sm:block hover:bg-gray-100 hover:opacity-50 w-full h-full ">
@@ -632,7 +631,7 @@
         draggableItem(draggable0)
         const draggable1 = document.getElementById('draggable-items-1');
         draggableItem(draggable1)
-        
+        console.log( ' {{Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE'])}}' )
       </script>
 
     @livewire('notification.contact')
