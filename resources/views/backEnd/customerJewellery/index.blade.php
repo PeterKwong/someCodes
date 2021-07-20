81fdc4ccf82c5f8e7c33e2673dbd19c30aa33a08
chillkwong <chillkwong@gmail.com>
2021-07-20 18:11:29 +0800

    2021_7_15g

--- resources/views/backEnd/customerJewellery/index.blade.php
+++ resources/views/backEnd/customerJewellery/index.blade.php
@@ -10,7 +10,7 @@
 				<tr @click="clickRow(props.item)">
 					<td>@{{props.item.id}}</td>
 					<td>@{{props.item.invoice_id}}</td>
-					<td v-if="props.item.texts">@{{props.item.texts[0].content}}</td>
+					<td v-if="props.item.texts[0]">@{{props.item.texts[0].content}}</td>
 					<td><img width="256" :src="props.item.images.length > 0 ? 
 						adminVar.storage[adminVar.storage.live] + 'public/images/'+ props.item.images[0].image:'' "></td>
 					<td>@{{props.item.published}}</td>
