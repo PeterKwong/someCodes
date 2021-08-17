38eb7d31e7873bad9b935c8c5cea6a654064e373
chillkwong <chillkwong@gmail.com>
2021-08-17 14:04:55 +0800

    2021_7_15g

--- resources/views/layouts/frontEnd/header.blade.php
+++ resources/views/layouts/frontEnd/header.blade.php
@@ -1,10 +1,10 @@
 <header class="fixed top-0 bg-white z-50 w-full bg-white py-2 lg:py-0 shadow-bottom" id="td-header">
     <!-- Header for Desktop  -->
      
-    @include('layouts.frontend.header-desktop')
+    @include('layouts.frontEnd.header-desktop')
 
-    @include('layouts.frontend.header-mobile')
-
+    @include('layouts.frontEnd.header-mobile')
+esfsfd
 
     <!-- <div x-data="{ cartOpen : false}" class="flex flex-col lg:hidden">
         <div x-show="cartOpen" class="sidebar">
@@ -24,3 +24,5 @@
 <div class="mb-16 lg:mb-52" id="header" ></div>
 
 @livewire('notification.flash')
+
+sudo chmod -R 777 /var/www/laravel/storage/logs
