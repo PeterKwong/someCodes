b2117a91407e664e077b3a230f1a2d1f7cffcf3d
chill kwong <chillkwong@gmail.com>
2021-08-22 01:08:20 +0800

    2021_3_20

--- resources/views/livewire/wedding-ring/index.blade.php
+++ resources/views/livewire/wedding-ring/index.blade.php
@@ -267,52 +267,53 @@
             @foreach($model['data'] as $data)
             @if( count($data['images']) )
             <div class="flex flex-col relative product-card rounded-none font-lato p-0 md:p-2 2xl:p-5 cursor-pointer transform hover:-translate-y-2 transition hover:border border-gold-light duration-500">
-                <a href="/{{app()->getLocale() . '/wedding-rings/' . $data['id'] }}" target="_blank" class="flex justify-center" href="./wedding-ring-details.html"><img class="mt-5 md:mt-0" src="{{ config('global.cache.' . config('global.cache.live') ) . 'public/images/' . $data['images'][0]['image']}}" alt="p1">
-                <div class="flex flex-col items-center space-y-2 md:space-y-3">
-                    <div class="flex flex-col md:flex-row items-center w-full md:w-auto px-2 md:px-0 md:justify-center md:space-x-3">
-                        <div class="flex flex-row justify-between md:justify-start w-full md:w-auto md:flex-col space-y-0 items-center">
-                            <span class="text-grey-04 text-sm">{{__('weddingRing.' . $data['wedding_rings'][0]['gender'])}}</span>
-                            <p class="flex items-center space-x-1 font-suranna text-gold-light">
-                                <span class="text-base md:text-xl">HKD</span>
-                                <span class="text-xl md:text-2xl">${{$data['wedding_rings'][0]['unit_price']}}</span>
-                            </p>
+                <a href="/{{app()->getLocale() . '/wedding-rings/' . $data['id'] }}" target="_blank" class="flex justify-center" ><img class="mt-5 md:mt-0" src="{{ config('global.cache.' . config('global.cache.live') ) . 'public/images/' . $data['images'][0]['image']}}" alt="p1"></a>
+                 <a href="/{{app()->getLocale() . '/wedding-rings/' . $data['id'] }}" target="_blank">
+                    <div class="flex flex-col items-center space-y-2 md:space-y-3">
+                        <div class="flex flex-col md:flex-row items-center w-full md:w-auto px-2 md:px-0 md:justify-center md:space-x-3">
+                            <div class="flex flex-row justify-between md:justify-start w-full md:w-auto md:flex-col space-y-0 items-center">
+                                <span class="text-grey-04 text-sm">{{__('weddingRing.' . $data['wedding_rings'][0]['gender'])}}</span>
+                                <p class="flex items-center space-x-1 font-suranna text-gold-light">
+                                    <span class="text-base md:text-xl">HKD</span>
+                                    <span class="text-xl md:text-2xl">${{$data['wedding_rings'][0]['unit_price']}}</span>
+                                </p>
+                            </div>
+                            <div class="flex flex-row justify-between md:justify-start w-full md:w-auto md:flex-col space-y-0 items-center">
+                                <span class="text-grey-04 text-sm">{{__('weddingRing.' . $data['wedding_rings'][1]['gender'])}}</span>
+                                <p class="flex items-center space-x-1 font-suranna text-gold-light">
+                                    <span class="text-base md:text-xl">HKD</span>
+                                    <span class="text-xl md:text-2xl">${{$data['wedding_rings'][1]['unit_price']}}</span>
+                                </p>
+                            </div>
                         </div>
-                        <div class="flex flex-row justify-between md:justify-start w-full md:w-auto md:flex-col space-y-0 items-center">
-                            <span class="text-grey-04 text-sm">{{__('weddingRing.' . $data['wedding_rings'][1]['gender'])}}</span>
-                            <p class="flex items-center space-x-1 font-suranna text-gold-light">
-                                <span class="text-base md:text-xl">HKD</span>
-                                <span class="text-xl md:text-2xl">${{$data['wedding_rings'][1]['unit_price']}}</span>
-                            </p>
+                        <h1 class="text-center text-lg md:text-xl font-suranna">
+                            <p>{{__('weddingRing.'. $data['wedding_rings'][0]['metal'])}} {{__('weddingRing.Wedding Ring')}}</p>
+                            <p class="text-xs font-lato">{{ $data['wedding_rings'][0]['title'] }}</p>
+                        </h1>
+                    </div>
+                    @if($data['wedding_rings'][0]['invoices_count'] != 0)
+                        <div class="flex items-center justify-center space-x-1 mt-3">
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.63604 0.5L11.0727 5.43763L16.5218 6.22964L12.5788 10.0728L13.5096 15.5L8.63604 12.9378L3.76224 15.5L4.69302 10.0728L0.75 6.22964L6.19914 5.43763L8.63604 0.5Z" fill="#EFCE4A"/>
+                            </svg>
+                            <span class="text-sm font-lato">{{$data['wedding_rings'][0]['invoices_count']}}</span>
                         </div>
-                    </div>
-                    <h1 class="text-center text-lg md:text-xl font-suranna">
-                        <a href="/{{app()->getLocale() . '/wedding-rings/' . $data['id'] }}" target="_blank">{{__('weddingRing.'. $data['wedding_rings'][0]['metal'])}} {{__('weddingRing.Wedding Ring')}}</a>
-                        <p class="text-xs font-lato">{{ $data['wedding_rings'][0]['title'] }}</p>
-                    </h1>
-                </div>
-                @if($data['wedding_rings'][0]['invoices_count'] != 0)
-                    <div class="flex items-center justify-center space-x-1 mt-3">
-                        <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8.63604 0.5L11.0727 5.43763L16.5218 6.22964L12.5788 10.0728L13.5096 15.5L8.63604 12.9378L3.76224 15.5L4.69302 10.0728L0.75 6.22964L6.19914 5.43763L8.63604 0.5Z" fill="#EFCE4A"/>
-                        </svg>
-                        <span class="text-sm font-lato">{{$data['wedding_rings'][0]['invoices_count']}}</span>
-                    </div>
-                @endif
+                    @endif
 
-                <div class="flex flex-col md:flex-row items-center justify-center divide-y md:divide-y-0 md:divide-x divide-gold divide-opacity-50 mx-3 md:mx-0 py-2 md:py-5">
-                    <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
-                        <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Shape')}}</p>
-                        <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['shape'])}}</p>
+                    <div class="flex flex-col md:flex-row items-center justify-center divide-y md:divide-y-0 md:divide-x divide-gold divide-opacity-50 mx-3 md:mx-0 py-2 md:py-5">
+                        <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
+                            <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Shape')}}</p>
+                            <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['shape'])}}</p>
+                        </div>
+                        <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
+                            <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Finish')}}</p>
+                            <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['finish'])}}</p>
+                        </div>
+                        <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
+                            <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Metal')}}</p>
+                            <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['metal'])}}</p>
+                        </div>
                     </div>
-                    <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
-                        <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Finish')}}</p>
-                        <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['finish'])}}</p>
-                    </div>
-                    <div class="relative flex w-full flex-row justify-between md:flex-col items-center md:space-y-3 py-2 md:px-3 2xl:px-4">
-                        <p class="text-xs md:text-sm text-black opacity-50 text-center">{{__('weddingRing.Metal')}}</p>
-                        <p class="text-xs md:text-sm text-center">{{__('weddingRing.'. $data['wedding_rings'][0]['metal'])}}</p>
-                    </div>
-                </div>
                 </a>
             </div>
             @endif
