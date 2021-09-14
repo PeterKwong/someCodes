81d847868650e5d516ef22665e75ac1676c44278
chillkwong <chillkwong@gmail.com>
2021-09-14 18:27:40 +0800

    2021_7_15gg

--- resources/views/layouts/js/screen-width.blade.php
+++ resources/views/layouts/js/screen-width.blade.php
@@ -1,5 +1,5 @@
-<!-- <script type="text/javascript">
+<script type="text/javascript">
 	if ({{isset($_COOKIE['screenX'])?0:1}}) {
 		setCookie('screenX',screen.width,300)
 	}
-</script> -->
+</script>
