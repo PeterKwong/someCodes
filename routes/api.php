c7eef23b1601646cd0bedcd74fa08f8f9509fd7e
chillkwong <chillkwong@gmail.com>
2021-09-14 18:10:48 +0800

    2021_7_15gg

--- routes/api.php
+++ routes/api.php
@@ -209,7 +209,7 @@ Route::get('cookies', [CookiesController::class,'index']);
 //payment
 Route::post('payment-cn-callback', [PurchasesController::class,'callbackByWechat']);
 
-Route::middleware('')->group(function(){
+Route::prefix('')->group(function(){
 	// dd(request()->user());
 	Route::get('account/user', [AccountController::class,'getUserInfo']);
 	Route::post('account/user', [AccountController::class,'userUpdate']);
