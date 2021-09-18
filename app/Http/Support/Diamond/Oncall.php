e7fc070c12c7e3281a4161c136cc94c8ea4882df
chillkwong <chillkwong@gmail.com>
2021-09-18 15:23:00 +0800

    2021_9_15gg

--- app/Http/Support/Diamond/Oncall.php
+++ app/Http/Support/Diamond/Oncall.php
@@ -138,7 +138,7 @@ trait Oncall{
 
     public function importDiamondFromAPI_1000_PerBatch($countingPage, $selectedID = 'white_diamond'){
           $data = $this->guzzleRequest($this->diamondSource,$selectedID,$countingPage);
-          dd($data);
+          dd(env('OCID'));
           $extractedDiamonds = $this->importDiamondsFromWebJson($data,$selectedID);   
 
           return $extractedDiamonds;
